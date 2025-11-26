8bed2f701d023aea6b9966db487b82cd7e29abbe
DAVI LAURINDO SILVA DE ARAUJO <2023320434@MACEIO.IFAL>
2025-11-26 09:43:19 -0300

    Pop

--- pitao2.py
+++ pitao2.py
@@ -8,11 +8,11 @@ pyautogui.press('win')
 pyautogui.write('chrome')
 pyautogui.press('enter')
 
-time.sleep(2)  # Give user time to switch to the target window
+time.sleep(2)
 webbrowser.open("https://www.wikipedia.org")
 time.sleep(1)
 pyautogui.typewrite('Python programming language', interval=0.1)
 pyautogui.press('enter')
-time.sleep(1)  # Wait for the page to load
+time.sleep(1) 
 img = pyautogui.screenshot()
 img.save('wikipedia_python.png')
